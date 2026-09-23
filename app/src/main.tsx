64ccec130a16130479cc6e6d3a68823787642357
myplexscripts <contactdavidbusch@gmail.com>
2026-09-22 22:26:31 -0400

    Use bundled reactive gem TypeScript module

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createIcons } from 'lucide';
-import '../../reactive-gem-system.js';
+import './gems/reactive-system';
 import './react-app.css';
 import App from './App';
 
